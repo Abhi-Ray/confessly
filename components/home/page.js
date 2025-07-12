3e73a673dafe8648f19692fbd8ceaae6d3f2cc86
Abhi7247 <rayabhishek20112000@gmail.com>
2025-07-12 17:46:57 +0530

    colors

--- components/home/page.js
+++ components/home/page.js
@@ -23,13 +23,13 @@ function timeAgo(dateString) {
     if (seconds < 60) return 'Just now';
     
     const minutes = Math.floor(seconds / 60);
-    if (minutes < 60) return `in ${minutes}m`;
+    if (minutes < 60) return `${minutes}m`;
     
     const hours = Math.floor(minutes / 60);
-    if (hours < 24) return `in ${hours}h`;
+    if (hours < 24) return `${hours}h`;
     
     const days = Math.floor(hours / 24);
-    if (days < 7) return `in ${days}d`;
+    if (days < 7) return `${days}d`;
     
     return 'Just now';
   }
